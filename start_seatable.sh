529852fc4757498c22cda9da9a55a988310ed653
Jim Zucker <jim@stratengllc.com>
2020-02-03 12:02:22 -0800

    Update localhost

--- start_seatable.sh
+++ start_seatable.sh
@@ -4,8 +4,11 @@ mkdir -p seatable_home
 mkdir -p seatable_home/seatable-data
 mkdir -p seatable_home/mysql-data
 docker-compose up -d
-sleep 120
+
+sleep 60
 docker exec -d seatable /shared/seatable/scripts/seatable.sh start
+
+set +x
 echo "
 ---------------------------------------------------------------------
 System is ready, remmeber first time you have to setup a super user on a new installation
