e8f391820cc5eeb6048e2eec43904ba0586af022
Jim Zucker <jim@stratengllc.com>
2020-02-03 11:42:44 -0800

    updated start script

--- start_seatable.sh
+++ start_seatable.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 set -ex
+mkdir -p seatable_home
 mkdir -p seatable_home/seatable-data
 mkdir -p seatable_home/mysql-data
 docker-compose up -d
 sleep 120
 docker exec -d seatable /shared/seatable/scripts/seatable.sh start
-sleep 60
 echo "
 ---------------------------------------------------------------------
 System is ready, remmeber first time you have to setup a super user on a new installation
