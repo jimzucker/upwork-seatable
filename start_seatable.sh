da6555b711871ccc8065976ef3eec6b6c67afd4b
Jim Zucker <jim@stratengllc.com>
2020-02-03 12:22:36 -0800

    Update stqrt script

--- start_seatable.sh
+++ start_seatable.sh
@@ -8,4 +8,18 @@ mkdir -p $STRATDEVOPS_SEATABLE_MYSQL
 mkdir -p $STRATDEVOPS_SEATABLE_DATA
 
 docker-compose up -d
+
+set +x
+echo "
+1. Verify server started with:
+docker logs seatable
+
+2. Wait 1 min then run:
+docker exec -d seatable /shared/seatable/scripts/seatable.sh start
+
+---------------------------------------------------------------------
+3. System is ready, remmeber first time you have to setup a super user on a new installation
+docker exec -it seatable /shared/seatable/scripts/seatable.sh superuser
+---------------------------------------------------------------------
+"
 exit 0
